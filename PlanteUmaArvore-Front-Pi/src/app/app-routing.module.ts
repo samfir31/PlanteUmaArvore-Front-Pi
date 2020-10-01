2da57737117e55b95a17bbdb1914d8e21310bf8d
unknown <samfir31@gmail.com>
2020-10-01 19:24:41 -0300

    Alterado visual do footer, navabar e componete sobre

--- PlanteUmaArvore-Front-Pi/src/app/app-routing.module.ts
+++ PlanteUmaArvore-Front-Pi/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'sobre', component: SobreComponent
+  },
+  {
+    path: 'contato', component: ContatoComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
