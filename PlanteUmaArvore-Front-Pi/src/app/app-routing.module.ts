4f6f66efd5238d42f067f8c1eb43434935728201
unknown <samfir31@gmail.com>
2020-10-07 08:55:19 -0300

    Alteração no layout do projeto, Home, Cadastro, Sobre

--- PlanteUmaArvore-Front-Pi/src/app/app-routing.module.ts
+++ PlanteUmaArvore-Front-Pi/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { ContatoComponent } from './contato/contato.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   { path:'', redirectTo: 'home', pathMatch: 'full' },
--- PlanteUmaArvore-Front-Pi/src/app/home/home.component.ts
+++ PlanteUmaArvore-Front-Pi/src/app/home/home.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+  
 })
 export class HomeComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+/* animação linha 1*/
+function animeScroll1(){  
+  const windowstop = window.pageYOffset;
+  
+    document.querySelectorAll('[data-anime1]').forEach(function(elemento){ 
+      if(windowstop>300){      
+      elemento.classList.add('animacao');
+    }
+    
+  
+  else{        
+      elemento.classList.remove('animacao');
+    }
+    if(windowstop>850){      
+      elemento.classList.remove('animacao');
+    } 
+  })
+}    
+  
+  
+window.addEventListener('scroll', function(){
+animeScroll1();
+})
+
+/* /animação linha 1*/
+
+/* animação linha 2*/
+function animeScroll2(){  
+      const windowstop = window.pageYOffset;          
+      
+        document.querySelectorAll('[data-anime]').forEach(function(elemento){  
+          if(windowstop>900){      
+          elemento.classList.add('animacao');
+        }
+          else{        
+          elemento.classList.remove('animacao');
+        } 
+        if(windowstop>1300){      
+          elemento.classList.remove('animacao');
+        }
+      })
+      
+
+    }    
+      
+      
+window.addEventListener('scroll', function(){
+    animeScroll2();
+})
+/* /animação linha 2*/
